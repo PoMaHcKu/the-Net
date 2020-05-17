3845a8c60dacecf3166a4e1acc30fd58d59548b5
Roman <rom2517pop@gmail.com>
2020-05-17 18:47:55 +0200

    added and tuning model mapper library

--- src/main/java/com/roman/thenetapi/config/AppConfig.java
+++ src/main/java/com/roman/thenetapi/config/AppConfig.java
@@ -1,4 +1,22 @@
 package com.roman.thenetapi.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mp = new ModelMapper();
+        mp.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return mp;
+    }
+
 }
