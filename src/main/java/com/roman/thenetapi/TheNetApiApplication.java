3d18931bbb0b7084537af895c41a1d36a724cc48
Roman <rom2517pop@gmail.com>
2020-05-17 17:41:44 +0200

    First commit. Created project

---
+++ src/main/java/com/roman/thenetapi/TheNetApiApplication.java
@@ -0,0 +1,13 @@
+package com.roman.thenetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNetApiApplication.class, args);
+	}
+
+}
